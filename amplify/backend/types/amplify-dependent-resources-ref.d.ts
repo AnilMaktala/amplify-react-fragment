61507bd5f668b7d8b2978bc2b950407b6a8f8623
Anil Maktala <amaktala@amazon.com>
2024-01-23 21:04:06 -0500

    amplify changes

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,9 @@
+export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "reactfragment": {
+      "GraphQLAPIEndpointOutput": "string",
+      "GraphQLAPIIdOutput": "string",
+      "GraphQLAPIKeyOutput": "string"
+    }
+  }
+}
